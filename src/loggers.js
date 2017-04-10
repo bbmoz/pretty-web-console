685153406c94f8d249dc7756409d253057b3bc9d
Joe Lee <blueberrymozart@gmail.com>
2017-04-09 22:06:38 -0500

    refactor: remove duplication of loggers

--- src/loggers.js
+++ src/loggers.js
@@ -1,26 +1,20 @@
 function loggers (store) {
+  function out (defaultPrint) {
+    return (val, print) => {
+      if (print) {
+        print(`%c${val}`, store.style)
+      } else {
+        defaultPrint(`%c${val}`, store.style)
+      }
+      store.style = ''
+    }
+  }
+
   return {
-    log, warn, error, info
-  }
-
-  function log (val, log = console.log) {
-    log(`%c${val}`, store.style)
-    store.style = ''
-  }
-
-  function warn (val, warn = console.warn) {
-    warn(`%c${val}`, store.style)
-    store.style = ''
-  }
-
-  function error (val, error = console.error) {
-    error(`%c${val}`, store.style)
-    store.style = ''
-  }
-
-  function info (val, info = console.info) {
-    info(`%c${val}`, store.style)
-    store.style = ''
+    log: out(console.log),
+    warn: out(console.warn),
+    error: out(console.error),
+    info: out(console.info)
   }
 }
 
